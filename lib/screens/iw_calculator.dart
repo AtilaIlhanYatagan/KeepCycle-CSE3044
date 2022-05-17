8f99d310b8b04a0e660858a2f14e9e117e5503d5
ibrahimhakkicandan <kartaliza113@gmail.com>
2022-05-17 18:34:25 +0300

    Bugs Fixed

--- lib/screens/iw_calculator.dart
+++ lib/screens/iw_calculator.dart
@@ -25,9 +25,15 @@ class _IWCalculatorState extends State<IWCalculator> {
     }
     return 0;
   }
+  bool isNumeric(String s) {
+    if (s == null) {
+      return false;
+    }
+    return int.tryParse(s) != null;
+  }
 
   final TextEditingController _heightController = TextEditingController();
-
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,93 +43,115 @@ class _IWCalculatorState extends State<IWCalculator> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Center(
-                child: Container(
-                    padding: const EdgeInsets.only(bottom: 10.0),
-                    child: Text(
-                        "Calculated Ideal Weight: " +
-                            _idealWeight.toStringAsFixed(1),
-                        style: const TextStyle(fontSize: 22))),
-              ),
-              TextField(
-                controller: _heightController,
-                decoration: InputDecoration(
-                    hintText: 'Enter your height here (Ex:180)',
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        _heightController.clear();
-                      },
-                      icon: const Icon(Icons.delete),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Center(
+                  child: Container(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Text(
+                          "Calculated Ideal Weight: " +
+                              _idealWeight.toStringAsFixed(1),
+                          style: const TextStyle(fontSize: 22))),
+                ),
+                TextFormField(
+                  controller: _heightController,
+                  decoration: InputDecoration(
+                      hintText: 'Enter your height here (Ex:180)',
+                      suffixIcon: IconButton(
+                        onPressed: () {
+
+                          _heightController.clear();
+                        },
+                        icon: const Icon(Icons.delete),
+                      ),
+                      border: const OutlineInputBorder(),
+                      fillColor: Colors.lightBlue.shade100,
+                      filled: true),
+                  keyboardType: TextInputType.number,
+                  maxLength: 3,
+                  validator: (value){
+                    if(value != null){
+                      if(value.isEmpty){
+                        return "Height can not empty!";
+                      }
+                      if(!isNumeric(value)){
+                        return "Only numbers allowed!";
+                      }
+                    }
+                  } ,
+                ),
+                CheckboxListTile(
+                    title: const Text('male'),
+                    secondary: const Icon(Icons.male),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    tileColor: Colors.lightBlue.shade100,
+                    value: _male,
+                    onChanged: (value) {
+                      setState(() {
+                        _male = value!;
+                        _female = false;
+                      });
+                    }),
+                CheckboxListTile(
+                    title: const Text('female'),
+                    secondary: const Icon(Icons.female),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    tileColor: Colors.lightBlue.shade100,
+                    value: _female,
+                    onChanged: (value) {
+                      setState(() {
+                        _female = value!;
+                        _male = false;
+                      });
+                    }),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ElevatedButton.icon(
+                          label: const Text('Calculate'),
+                          icon: const Icon(Icons.add, color: Colors.white),
+                          onPressed: //(_weightController.text.isEmpty || _heightController.text.isEmpty)  ? null :
+                              () {
+                               final isProper = formKey.currentState?.validate();
+                               if(isProper == true) {
+                                 setState(() {
+                                   _height = int.parse(_heightController.text);
+                                   // _bodyWeight = int.parse(_weightController.text);
+                                   if(_female == null && _male == null ){
+                                      print("Please select Male or Female");
+                                   }
+                                   _idealWeight =
+                                       calculateIW(_height, _male, _female);
+                                 });
+                               }
+                          }),
+
                     ),
-                    border: const OutlineInputBorder(),
-                    fillColor: Colors.lightBlue.shade100,
-                    filled: true),
-                keyboardType: TextInputType.number,
-                maxLength: 3,
-              ),
-              CheckboxListTile(
-                  title: const Text('male'),
-                  secondary: const Icon(Icons.male),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  tileColor: Colors.lightBlue.shade100,
-                  value: _male,
-                  onChanged: (value) {
-                    setState(() {
-                      _male = value!;
-                      _female = false;
-                    });
-                  }),
-              CheckboxListTile(
-                  title: const Text('female'),
-                  secondary: const Icon(Icons.female),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  tileColor: Colors.lightBlue.shade100,
-                  value: _female,
-                  onChanged: (value) {
-                    setState(() {
-                      _female = value!;
-                      _male = false;
-                    });
-                  }),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10.0),
-                    child: ElevatedButton.icon(
-                        label: const Text('Calculate'),
-                        icon: const Icon(Icons.add, color: Colors.white),
-                        onPressed: //(_weightController.text.isEmpty || _heightController.text.isEmpty)  ? null :
-                            () {
-                          setState(() {
-                            _height = int.parse(_heightController.text);
-                            // _bodyWeight = int.parse(_weightController.text);
-                            _idealWeight = calculateIW(_height, _male, _female);
-                          });
-                        }),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(10.0),
-                    child: ElevatedButton.icon(
-                        label: const Text('Reset'),
-                        icon: const Icon(Icons.clear, color: Colors.red),
-                        onPressed: //(_weightController.text.isEmpty || _heightController.text.isEmpty)  ? null :
-                            () {
-                          setState(() {
-                            _height = 0;
-                            _heightController.clear();
-                            _male = false;
-                            _female = false;
-                          });
-                        }),
-                  ),
-                ],
-              ),
-            ],
+                    Container(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ElevatedButton.icon(
+                          label: const Text('Reset'),
+                          icon: const Icon(Icons.clear, color: Colors.red),
+                          onPressed: //(_weightController.text.isEmpty || _heightController.text.isEmpty)  ? null :
+                              () {
+                            setState(() {
+                              _height = 0;
+                              _heightController.clear();
+                              _male = false;
+                              _female = false;
+                            });
+                          }),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ));
   }
